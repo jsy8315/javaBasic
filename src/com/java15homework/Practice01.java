22351da5c7f33e335951202109b1a6de34e01197
Suyoung Jung <110025650+jsy8315@users.noreply.github.com>
2023-03-20 17:02:58 +0900

    15강 예외 연습문제 15-2

--- src/com/java15homework/Practice01.java
+++ src/com/java15homework/Practice01.java
@@ -2,8 +2,18 @@ package com.java15homework;
 
 public class Practice01 {
 	public static void printLength(String s) {
+		try {
 		int lengthOfS = s.length();
 		System.out.println("문자 수: " + lengthOfS);
+	} catch(NullPointerException e) {
+		System.out.println("NullPointerException 예외를 catch 하였습니다");
+		//"NullPointerException 예외를 catch 하였습니다"를 표시한다
+		System.out.println("----stack trace(여기부터)----");
+		// ----stack trace(여기부터)----를 표시한다
+		e.printStackTrace();
+		// stack trace를 표시한다
+		System.out.println("----stack trace(여기까지)----");
+	}
 	}
 	public static void main(String[] args) {
 		printLength("라면잉건가"); // 비교하기 위한 test
