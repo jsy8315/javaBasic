8edbc9af0c0d12dec6f4a0517a5b73eb25dcfd30
Suyoung Jung <110025650+jsy8315@users.noreply.github.com>
2023-03-21 17:50:30 +0900

    응용 05강 람다식 연습문제 5-2

--- src/com/javaApplication05homework/Utils.java
+++ src/com/javaApplication05homework/Utils.java
@@ -21,11 +21,11 @@ public class Utils {
 	}
 	
 	public static void main(String[] args) {
-		Func1 func1 = Utils::isOdd;
+		Func1 func1 = (int n) -> {return n % 2 == 1;};
 		boolean result1 = func1.func1IsOdd(5);
 		System.out.println("5 % 2 == 1 : " + result1);
 		
-		Func2 func2 = Utils::addNamePrefix;		
+		Func2 func2 = (boolean male, String name) -> { if (male == true) {return "Mr." + name;} return "Ms." + name;};	
 		String result2 = func2.func2AddNamePrefix(true, "Suyoung");
 		System.out.println(result2);
 	}
